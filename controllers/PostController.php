7c868b074010e169f1a5b104174bbae6ceb68152
lis <lismagol@gmail.com>
2019-12-02 21:23:56 -0300

    Adicionando validações ao cadastro de post

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -48,17 +48,59 @@ class PostController
     {
         $user = $_POST['user_id'];
         $description = $_POST['description'];
-        $fileName = $_FILES['image']['name'];
-        $tmpTarget = $_FILES['image']['tmp_name'];
-        $targetFile = "views/img/posts/$fileName";
-        move_uploaded_file($tmpTarget,$targetFile);
+        $fieldname = "image";
+        $targetDir = "views/img/posts/";
+        $fileName = $_FILES[$fieldname]['name'];
+        $fileExtension = pathinfo($fileName, PATHINFO_EXTENSION);
+        $fileSize = $_FILES[$fieldname]['size'];
+        $fileType = $_FILES[$fieldname]['type'];
+        $tempDir = $_FILES[$fieldname]['tmp_name'];
+        //Lista de tipos de arquivos permitidos como imagem do produto
+        $allowedTypes = array('image/svg', 'image/jpeg', 'image/jpg', 'image/png');
+        //Tamanho máximo permitido da imagem (em bytes)
+        $allowedSize = 1024 * 500; //500Kb
+        $uploadOk = 1;
 
-        $post = new Post();
-        $result = $post->createPost($targetFile,$description,$user);
-
-        if($result) {
-            header('Location:/desafio-OOP-PHP/posts');
+        if(iconv_strlen($description) > 140) {
+            echo "Descrição muito longa. O máximo de caracteres é 140.<br>";
+            $uploadOk = 0;
         }
+        //Valida se a imagem foi selecionada
+		if (isset($fileName)) {
+			//Renomeia a imagem: data do envio_nome do arquivo
+			$newFileName = date("d-M-Y_").$fileName.'.'.$fileExtension;
+			$targetFile = $targetDir.$newFileName;
+
+			//Verifica se já existe uma imagem com o mesmo nome
+			if (file_exists($targetFile)) {
+				echo "Já existe um arquivo com esse nome. Por favor, use outro nome.<br>";
+                $uploadOk = 0;
+			
+			//Verifica o formato do arquivo e compara com uma lista de extensões pré-definidas
+			} elseif (array_search($fileType, $allowedTypes) === false) {
+				echo "O tipo de arquivo enviado é inválido. Somente imagens são aceitas.<br>";
+                $uploadOk = 0;
+			
+			//Verifica o tamanho do arquivo e compara com o máximo pré-estabelecido
+			} elseif ($fileSize > $allowedSize) {
+				echo "O tamanho do arquivo enviado é maior que o limite. Por favor, diminua o tamanho da imagem.<br>";
+				$uploadOk = 0;
+
+			//Última verificação para checar se algum erro deixou $uploadOk igual a 0
+			} elseif ($uploadOk == 0) {
+				echo "Não foi possível cadastrar o post.";
+			
+			//Se tudo estiver ok, salva a imagem
+			} else {
+				if (move_uploaded_file($tempDir, $targetFile)) {
+					$post = new Post();
+                    $post->createPost($targetFile,$description,$user);
+                    header('Location:/desafio-OOP-PHP/posts');
+				} else  {
+					echo "Você deve selecionar uma imagem para salvar.";
+				}
+			}
+		}
     }
 
     private function showPosts()
