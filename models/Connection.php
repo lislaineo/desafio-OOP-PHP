a6a3c1a6dc5f57d2806836c5b75b3d33ce448570
lis <lismagol@gmail.com>
2019-12-03 16:22:17 -0300

    Inserção de comentários nos models

--- models/Connection.php
+++ models/Connection.php
@@ -1,17 +1,12 @@
-<?php 
-
+<?php
 class Connection
 {
-    private $host = 'mysql:host=localhost;dbname=fakeinsta;port=3306;charset=utf8mb4';
-    private $user = 'root';
-    private $pass = '';
+  private $host = 'mysql:host=localhost;dbname=fakeinsta;port=3306;charset=utf8mb4';
+  private $user = 'root';
+  private $pass = '';
 
-    protected function createConnection()
-    {
-        return new PDO($this->host, $this->user, $this->pass);
-    }
-    
+  protected function createConnection()
+  {
+    return new PDO($this->host, $this->user, $this->pass);
+  }
 }
-
-
-?>
--- models/User.php
+++ models/User.php
@@ -1,24 +1,23 @@
-<?php 
+<?php
 include_once 'Connection.php';
 
+// classe User herda da Connection para conseguir conectar ao banco de dados
 class User extends Connection
 {
-    public function createUser($profilePic,$username,$pass,$name,$email)
-    {
-        $dsn = parent::createConnection();
-        $query = $dsn->prepare('INSERT INTO users (image, login, password, name, email) values (?,?,?,?,?)');
-        return $query->execute([$profilePic,$username,$pass,$name,$email]);
-    }
+  // método de criação de usuário (insere dados do usuário no banco)
+  public function createUser($profilePic, $username, $pass, $name, $email)
+  {
+    $dsn = parent::createConnection();
+    $query = $dsn->prepare('INSERT INTO users (image, login, password, name, email) values (?,?,?,?,?)');
+    return $query->execute([$profilePic, $username, $pass, $name, $email]);
+  }
 
-    public function getUserInfo($username) 
-    {
-        $dsn = parent::createConnection();
-        $query = $dsn->query("SELECT * FROM users where login = '$username'");
-        $result = $query->fetchAll(PDO::FETCH_OBJ);
-        return $result;
-    }
-    
+  // método para verificar os dados do usuário (busca dados de login no banco e devolve para a página)
+  public function getUserInfo($username)
+  {
+    $dsn = parent::createConnection();
+    $query = $dsn->query("SELECT * FROM users where login = '$username'");
+    $result = $query->fetchAll(PDO::FETCH_OBJ);
+    return $result;
+  }
 }
-
-
-?>
